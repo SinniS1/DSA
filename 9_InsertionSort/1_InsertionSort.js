0bce9bb22e161c718680b5e60c4333c064621ef1
SinniS1 <sinniscode@gmail.com>
2023-04-03 00:49:56 +0530

    Insertion Sort done

---
+++ 9_InsertionSort/1_InsertionSort.js
@@ -0,0 +1,13 @@
+// Insertion Sort
+function InsertionSort(array) {
+	for (var i = 1; i < array.length; i++) {
+		var currentVal = array[i];
+		for (var j = i - 1; j >= 0 && array[j] > currentVal; j--) {
+			array[j + 1] = array[j];
+		}
+		array[j + 1] = currentVal;
+	}
+	return array;
+}
+
+console.log(InsertionSort([45, 74, 1, 2, 3, 58, 4, 5, 4, 2, 9, 6, 3]));

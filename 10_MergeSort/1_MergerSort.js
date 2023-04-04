e6d61c133904edef61b0a7eb246fff5f09d4c629
SinniS1 <sinniscode@gmail.com>
2023-04-04 23:54:19 +0530

    time complexity of merge sort

--- 10_MergeSort/1_MergerSort.js
+++ 10_MergeSort/1_MergerSort.js
@@ -5,7 +5,7 @@ function Merging(arr1, arr2) {
 	let i = 0;
 	let j = 0;
 	while (i < arr1.length && j < arr2.length) {
-		if (arr1[i] < arr2[j]) {
+		if (arr1[i] <= arr2[j]) {
 			NewArray.push(arr1[i]);
 			i++;
 		} else {
@@ -33,4 +33,12 @@ function Splitting(arr) {
 	return Merging(left, right);
 }
 
-console.log(Splitting([10, 24, 76, 73]));
+console.log(Splitting([10, 24, 76, 73, 1]));
+
+// Time compleity is O( n log n )
+
+// log n  -> it means rate of growth of splitting array
+// -> if we have 32 element array we need to split it 5 times, so 2 mutiply by 5 gives 32
+// -> if we have 16 element array we need to split it 3 times, so 2 mutiply by 3 gives 16
+
+// n    -> it comes from how many times we need to merge our array it will always be samee as the number of elements

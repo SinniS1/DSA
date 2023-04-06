400302e57aa30a2e7913dd9e57654b66960509c6
SinniS1 <sinniscode@gmail.com>
2023-04-06 23:48:44 +0530

    Radix Sort Start

---
+++ 12_RadixSort/12_RadixSort.js
@@ -0,0 +1,6 @@
+// Redix Sort
+// Visualize Radix Sort  https://visualgo.net/en/sorting?slide=16
+/* Radix sort is a non-comparative sorting algorithm that sorts elements by grouping them based on the digits (or other individual elements) that make up the values of the elements. 
+The algorithm sorts the elements by their individual digits, starting from the least significant digit to the most significant digit. 
+The digits are sorted by counting the frequency of occurrence of each digit in each element of the array and using this information to arrange the elements in the correct order. 
+*/

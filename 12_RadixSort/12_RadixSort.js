e1d732e4a8144afcdcff29f48ae41f28e75e2951
SinniS1 <sinniscode@gmail.com>
2023-04-07 23:30:36 +0530

    Radix Sort DigiCount func

--- 12_RadixSort/12_RadixSort.js
+++ 12_RadixSort/12_RadixSort.js
@@ -4,3 +4,20 @@
 The algorithm sorts the elements by their individual digits, starting from the least significant digit to the most significant digit. 
 The digits are sorted by counting the frequency of occurrence of each digit in each element of the array and using this information to arrange the elements in the correct order. 
 */
+
+function getPos(num, place) {
+	num = Math.abs(num);
+	var devisor = 1;
+	for (let i = 1; i <= place; i++) {
+		devisor *= 10;
+	}
+	return Math.floor(num / devisor) % 10;
+}
+
+function digitCount(num) {
+	num = Math.abs(Math.floor(num));
+	var value = num.toString();
+	return value.length;
+}
+
+// console.log(digitCount(-12.13));

0d7a867336d7247e771309a98217c6f4abbad166
SinniS1 <sinniscode@gmail.com>
2023-04-21 22:49:37 +0530

    merge sort documentaion

--- 10_MergeSort/1_MergerSort.js
+++ 10_MergeSort/1_MergerSort.js
@@ -1,40 +1,42 @@
-// Merge Sort
 // Merging function
 function Merging(arr1, arr2) {
-	var NewArray = [];
-	let i = 0;
-	let j = 0;
-	while (i < arr1.length && j < arr2.length) {
-		if (arr1[i] <= arr2[j]) {
-			NewArray.push(arr1[i]);
-			i++;
-		} else {
-			NewArray.push(arr2[j]);
-			j++;
+	var NewArray = []; // Create a new array to store the merged elements
+	let i = 0; // Create a pointer for the first array
+	let j = 0; // Create a pointer for the second array
+	while (i < arr1.length && j < arr2.length) { // Compare elements from both arrays until one of them is exhausted
+		if (arr1[i] <= arr2[j]) { // If element from the first array is smaller or equal
+			NewArray.push(arr1[i]); // Push it to the new array
+			i++; // Move the pointer of the first array to the next element
+		} else { // If element from the second array is smaller
+			NewArray.push(arr2[j]); // Push it to the new array
+			j++; // Move the pointer of the second array to the next element
 		}
 	}
+	// Copy any remaining elements from the first array
 	while (i < arr1.length) {
 		NewArray.push(arr1[i]);
 		i++;
 	}
+	// Copy any remaining elements from the second array
 	while (j < arr2.length) {
 		NewArray.push(arr2[j]);
 		j++;
 	}
-	return NewArray;
+	return NewArray; // Return the merged and sorted array
 }
 
 // Splitting function
 function Splitting(arr) {
-	if (arr.length <= 1) return arr;
-	let middle = Math.floor(arr.length / 2);
-	let left = Splitting(arr.slice(0, middle));
-	let right = Splitting(arr.slice(middle));
-	return Merging(left, right);
+	if (arr.length <= 1) return arr; // Base case: if array has 0 or 1 element, it is already sorted
+	let middle = Math.floor(arr.length / 2); // Find the middle index of the array
+	let left = Splitting(arr.slice(0, middle)); // Recursively split the left half of the array
+	let right = Splitting(arr.slice(middle)); // Recursively split the right half of the array
+	return Merging(left, right); // Merge the sorted left and right halves and return the sorted array
 }
 
 console.log(Splitting([10, 24, 76, 73, 1]));
 
+// Time complexity is O(n log n)
 // Time compleity is O( n log n )
 
 // log n  -> it means rate of growth of splitting array

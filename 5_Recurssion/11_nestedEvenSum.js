b8255fd08677b61647ae359c10180c2bd9f50c91
SinniS1 <sinniscode@gmail.com>
2023-04-23 23:39:32 +0530

    nestedevensum documentation

--- 5_Recurssion/11_nestedEvenSum.js
+++ 5_Recurssion/11_nestedEvenSum.js
@@ -43,3 +43,27 @@ var obj2 = {
 
 console.log(nestedEvenSum(obj1)); // 6
 nestedEvenSum(obj2); // 10
+
+
+/**
+ The nestedEvenSum function is a recursive JavaScript function that takes an object obj as input and calculates the sum of all even numeric values 
+ that are nested inside the object, including nested objects.
+
+Parameters
+obj: An object that needs to be searched for even numeric values.
+
+Return Value
+Returns the sum of all even numeric values found in the input obj.
+
+Function Implementation
+The nestedEvenSum function uses a helper function called helper to recursively traverse the nested objects and sum up the even numeric values.
+
+1. The helper function takes an array input as input, which is obtained from Object.values(obj), where obj is the input object passed to the nestedEvenSum function.
+2. If input is empty (i.e., !input.length is true), the function returns 0, as there are no more elements to process.
+3. If the first element of input is a number and it is even (determined using typeof input[0] === "number" && input[0] % 2 === 0), the value is added to the sum variable.
+4. If the first element of input is an object (determined using typeof input[0] === "object"), a new array newInput is created by calling Object.values(input[0]), and the helper function is recursively called with newInput as the argument. This step is used to recursively traverse the nested objects.
+5. The helper function is called recursively with the remaining elements of input (i.e., input.slice(1)) to continue traversing the remaining elements.
+6. The helper function is initially called with the array val, which contains the values of the input obj obtained using Object.values(obj).
+7. Once all the nested objects are traversed and even numeric values are added to the sum variable, the final sum is returned.
+ */
+

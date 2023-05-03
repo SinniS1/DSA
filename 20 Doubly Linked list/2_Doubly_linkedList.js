3961b8b6ca3c07fd341726880e4cbf275483daf1
SinniS1 <sinniscode@gmail.com>
2023-05-03 22:15:02 +0530

    Made a node and Doubly constructor

--- 20 Doubly Linked list/2_Doubly_linkedList.js
+++ 20 Doubly Linked list/2_Doubly_linkedList.js
@@ -1,2 +1,18 @@
 // Doubly LinkedList is type of linkedlist which does not have any tail and head.
 // Any node can be selected randomly to callout as head from which we can run our functions in it
+
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+		this.previou = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+}

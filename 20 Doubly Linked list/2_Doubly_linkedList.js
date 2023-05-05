1f6060565b38811d878e5fc679ac236e3b58c70b
SinniS1 <sinniscode@gmail.com>
2023-05-05 22:53:25 +0530

    fixed a bug

--- 20 Doubly Linked list/2_Doubly_linkedList.js
+++ 20 Doubly Linked list/2_Doubly_linkedList.js
@@ -16,12 +16,3 @@ class DoublyLinkedList {
 		this.length = 0;
 	}
 }
-/*
-
-1. Define a `Node` class that has three properties: `data`, `prev`, and `next`.
-2. Define a `DoublyLinkedList` class that has three properties: `head`, `tail`, and `size`.
-3. Initialize the `head` and `tail` to `null`, and the `size` to `0`.
-4. Define a `append` method that adds a new node to the end of the list. This method should create a new `Node` object with the given data, set its `prev` pointer to the current `tail`, set the `next` pointer of the current `tail` to the new `Node`, and finally set the `tail` to the new `Node`. Update the `size` of the list after adding the new node.
-5. Define a `prepend` method that adds a new node to the beginning of the list. This method should create a new `Node` object with the given data, set its `next` pointer to the current `head`, set the `prev` pointer of the current `head` to the new `Node`, and finally set the `head` to the new `Node`. Update the `size` of the list after adding the new node.
-*/
-

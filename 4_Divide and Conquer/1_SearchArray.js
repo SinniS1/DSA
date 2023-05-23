d2e72b784a0b4a778a424a3c98f7302e7177396d
Sachin <sinniscode@gmail.com>
2023-05-23 23:25:44 +0530

    refactored & algo

--- 4_Divide and Conquer/1_SearchArray.js
+++ 4_Divide and Conquer/1_SearchArray.js
@@ -28,3 +28,14 @@ console.log(search([1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300, 400, 500],
 console.log(search([1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300, 400, 500], 50));
 console.log(search([1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300, 400, 500], 300));
 console.log(search([1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300, 400, 500], 6));
+
+
+// Algorithm
+// Initialize left as 0, representing the leftmost index of the search space.
+// Initialize right as the index of the last element in the array, representing the rightmost index of the search space.
+// Enter a loop that continues as long as left is less than or equal to right.
+// Calculate middle as the integer division of the sum of left and right by 2, rounding down to the nearest whole number.
+// If the value at array[middle] is greater than val, update right to middle - 1, narrowing the search space to the left half.
+// If the value at array[middle] is less than val, update left to middle + 1, narrowing the search space to the right half.
+// If the value at array[middle] is equal to val, the search is successful, and the function returns middle.
+// If the loop concludes without finding a match, the function returns -1 to indicate that val is not present in the array.
